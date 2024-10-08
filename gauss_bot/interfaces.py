c092577d533f7a556e8b21a68abbc86554419689
jp-zuniga <jperez070705@gmail.com>
2024-10-08 13:54:25 -0600

    stylesheet actualizada

--- gauss_bot/interfaces.py
+++ gauss_bot/interfaces.py
@@ -11,6 +11,28 @@ from PyQt6.QtCore import Qt
 from matrices import OperacionesMatrices
 from vectores import OperacionesVectores
 
+STYLESHEET = """
+    QWidget {
+        background-color: #2c3e50;
+        color: #ecf0f1;
+    }
+    QLabel {
+        color: #ecf0f1;
+    }
+    QPushButton {
+        background-color: #3498db;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        padding: 10px;
+        font-size: 16px;
+    }
+    QPushButton:hover {
+        background-color: #2980b9;
+    }
+"""
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -18,15 +40,12 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("GaussBot")
         self.setGeometry(100, 100, 800, 600)
 
-        # Set up the central widget
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        # Set up the layout
         layout = QVBoxLayout()
         central_widget.setLayout(layout)
 
-        # Add the main menu widget
         main_menu_widget = MainMenuWidget()
         layout.addWidget(main_menu_widget)
 
@@ -35,90 +54,72 @@ class MainMenuWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.setWindowTitle("Menu Principal")
+        self.setWindowTitle("Menú Principal")
         self.setGeometry(100, 100, 400, 300)
 
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        # Title label
         title_label = QLabel("GaussBot")
-        title_label.setFont(QFont("Nunito", 24))
+        title_label.setFont(QFont("Nunito", 24, QFont.Weight.Bold))
         title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(title_label)
 
-        # Buttons with icons
-        matrices_button = QPushButton("Operaciones de matrices")
-        matrices_button.setIcon(QIcon(path.join('gauss_bot', 'icons', 'matrices.png')))
+        matrices_button = QPushButton("Menú de Matrices")
+        # matrices_button.setIcon(QIcon(path.join('gauss_bot', 'icons', 'matrices.png')))
         matrices_button.setStyleSheet("QPushButton { font-size: 18px; padding: 10px; }")
-        matrices_button.clicked.connect(self.matrices_operations)
+        matrices_button.clicked.connect(self.operaciones_matrices)
         layout.addWidget(matrices_button)
 
-        vectores_button = QPushButton("Operaciones de vectores")
-        vectores_button.setIcon(QIcon(path.join('gauss_bot', 'icons', 'vectores.png')))
+        vectores_button = QPushButton("Menú de Vectores")
+        # vectores_button.setIcon(QIcon(path.join('gauss_bot', 'icons', 'vectores.png')))
         vectores_button.setStyleSheet("QPushButton { font-size: 18px; padding: 10px; }")
-        vectores_button.clicked.connect(self.vectores_operations)
+        vectores_button.clicked.connect(self.operaciones_vectores)
         layout.addWidget(vectores_button)
 
-        close_button = QPushButton("Cerrar programa")
-        close_button.setIcon(QIcon(path.join('gauss_bot', 'icons', 'close.png')))
+        close_button = QPushButton("Cerrar")
+        # close_button.setIcon(QIcon(path.join('gauss_bot', 'icons', 'close.png')))
         close_button.setStyleSheet("QPushButton { font-size: 18px; padding: 10px; }")
         close_button.clicked.connect(self.close_program)
         layout.addWidget(close_button)
 
-        # Apply a stylesheet to the entire widget
-        self.setStyleSheet("""
-            QWidget {
-                background-color: #f0f0f0;
-            }
-            QLabel {
-                color: #333;
-            }
-            QPushButton {
-                background-color: #4CAF50;
-                color: white;
-                border: none;
-                border-radius: 5px;
-            }
-            QPushButton:hover {
-                background-color: #45a049;
-            }
-        """)
 
-    def matrices_operations(self):
-        self.matrix_menu = MenuMatriz()
-        self.matrix_menu.show()
+    def operaciones_matrices(self):
+        self.menu_matriz = MenuMatriz()
+        self.menu_matriz.show()
 
-    def vectores_operations(self):
-        self.vector_menu = MenuVector()
-        self.vector_menu.show()
+    def operaciones_vectores(self):
+        self.menu_vector = MenuVector()
+        self.menu_vector.show()
 
     def close_program(self):
         self.close()
 
+
 class MenuMatriz(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Matrix Operations")
+        self.setWindowTitle("Operaciones Matriciales")
         self.setGeometry(150, 150, 600, 400)
 
-        self.op_mat = OperacionesMatrices()
+        self.op_mats = OperacionesMatrices()
 
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        label = QLabel("Matrix Operations Menu")
-        label.setFont(QFont("Nunito", 20))
+        label = QLabel("Operaciones Matriciales")
+        label.setFont(QFont("Nunito", 20, QFont.Weight.Bold))
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(label)
 
         # Add buttons for matrix operations
         operations = [
-            ("Agregar matriz", self.mat.agregar_matriz),
-            ("Resolver sistema de ecuaciones", self.mat.resolver_sistema),
-            ("Suma y resta de matrices", self.mat.suma_resta_matrices),
-            ("Multiplicación de matrices", self.mat.mult_matrices),
-            ("Transposición de matrices", self.mat.transponer),
+            ("Mostrar matrices", self.op_mats.imprimir_matrices),
+            ("Agregar matriz", self.op_mats.agregar_matriz),
+            ("Resolver sistema de ecuaciones", self.op_mats.resolver_sistema),
+            ("Suma y resta de matrices", self.op_mats.suma_resta_matrices),
+            ("Multiplicación de matrices", self.op_mats.mult_matrices),
+            ("Transposición de matrices", self.op_mats.transponer),
             ("Regresar al menú principal", self.close)
         ]
 
@@ -127,25 +128,6 @@ class MenuMatriz(QWidget):
             button.setStyleSheet("QPushButton { font-size: 18px; padding: 10px; }")
             button.clicked.connect(op_method)
             layout.addWidget(button)
-
-        # Apply the same stylesheet as the main menu
-        self.setStyleSheet("""
-            QWidget {
-                background-color: #f0f0f0;
-            }
-            QLabel {
-                color: #333;
-            }
-            QPushButton {
-                background-color: #4CAF50;
-                color: white;
-                border: none;
-                border-radius: 5px;
-            }
-            QPushButton:hover {
-                background-color: #45a049;
-            }
-        """)
 
 
 class MenuVector(QWidget):
@@ -154,23 +136,23 @@ class MenuVector(QWidget):
         self.setWindowTitle("Operaciones Vectoriales")
         self.setGeometry(150, 150, 600, 400)
 
-        self.vec = OperacionesVectores()
+        self.op_vecs = OperacionesVectores()
 
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        label = QLabel("Menu Operaciones Vectoriales")
-        label.setFont(QFont("Nunito", 20))
+        label = QLabel("Operaciones Vectoriales")
+        label.setFont(QFont("Nunito", 20, QFont.Weight.Bold))
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(label)
 
-        # Add buttons for vector operations
         operations = [
-            ("Agregar un vector", self.vec.agregar_vector),
-            ("Suma y resta de vectores", self.vec.suma_resta_vectores),
-            ("Multiplicación escalar", self.vec.mult_escalar),
-            ("Producto punto", self.vec.mult_vectorial),
-            ("Producto matriz-vector", self.vec.mult_matriz_vector),
+            ("Mostrar vectores", self.op_vecs.imprimir_vectores),
+            ("Agregar vector", self.op_vecs.agregar_vector),
+            ("Suma y resta de vectores", self.op_vecs.suma_resta_vectores),
+            ("Multiplicación escalar", self.op_vecs.mult_escalar),
+            ("Producto punto", self.op_vecs.mult_vectorial),
+            ("Producto matriz-vector", self.op_vecs.mult_matriz_vector),
             ("Regresar al menú principal", self.close)
         ]
 
@@ -180,27 +162,10 @@ class MenuVector(QWidget):
             button.clicked.connect(op_method)
             layout.addWidget(button)
 
-        # Apply the same stylesheet as the main menu
-        self.setStyleSheet("""
-            QWidget {
-                background-color: #f0f0f0;
-            }
-            QLabel {
-                color: #333;
-            }
-            QPushButton {
-                background-color: #4CAF50;
-                color: white;
-                border: none;
-                border-radius: 5px;
-            }
-            QPushButton:hover {
-                background-color: #45a049;
-            }
-        """)
 
 if __name__ == "__main__":
     app = QApplication([])
+    app.setStyleSheet(STYLESHEET)
     window = MainWindow()
     window.show()
     app.exec()
