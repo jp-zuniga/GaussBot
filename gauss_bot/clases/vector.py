4b815d6b32858b7da5ca898d0d3f6ec6f1c647a8
jp-zuniga <jperez070705@gmail.com>
2024-10-12 14:49:09 -0600

    clase Vector documentada

--- gauss_bot/clases/vector.py
+++ gauss_bot/clases/vector.py
@@ -3,15 +3,41 @@ from typing import Union, overload
 
 
 class Vector:
-    def __init__(self, componentes: Union[list[Fraction], None]) -> None:
-        if componentes is None:
-            self._componentes = []
-        else:
-            self._componentes = componentes
+    """
+    Representa un vector matemático de cualquier dimensión.
+    Los componentes se almacenan en una lista de objetos Fraction().
+
+    Las operaciones vectoriales básicas estan definidas con dunder methods:
+    * __add__(): suma de vectores
+    * __sub__(): resta de vectores
+    * __mul__(): overloads para producto punto y producto por escalar
+
+    De tal manera que se pueda decir:
+    * a + b = Vector([a1 + b1, a2 + b2, ..., an + bn])
+    * a - b = Vector([a1 - b1, a2 - b2, ..., an - bn])
+    * a * b = a1*b1 + a2*b2 + ... + an*bn
+    * a * k = Vector([a1*k, a2*k, ..., an*k])
+
+    Donde a y b sean objetos Vector() y k sea un objeto Fraction().
+
+    También se implementan los dunder methods:
+    * __getitem__(): para acceder a los componentes del vector
+    * __eq__(): para comparar vectores
+    * __len__(): para obtener la longitud del vector
+    * __str__(): para representar el vector como string
+    """
+
+    def __init__(self, componentes: list[Fraction]) -> None:
+        self._componentes = componentes
 
     @property
     def componentes(self) -> list[Fraction]:
         return self._componentes
+
+    def __getitem__(self, indice: int) -> Fraction:
+        if indice >= len(self):
+            raise IndexError("Índice inválido!")
+        return self.componentes[indice]
 
     def __eq__(self, vec2: object) -> bool:
         if not isinstance(vec2, Vector):
@@ -22,16 +48,6 @@ class Vector:
 
     def __len__(self) -> int:
         return len(self.componentes)
-
-    def __getitem__(self, indice: int) -> Fraction:
-        if indice >= len(self):
-            raise IndexError("Índice inválido!")
-        return self.componentes[indice]
-
-    def __setitem__(self, indice: int, valor: Fraction) -> None:
-        if indice >= len(self):
-            raise IndexError("Índice inválido!")
-        self.componentes[indice] = valor
 
     def __str__(self) -> str:
         return f"[{', '.join(str(c) for c in self.componentes)}]"
@@ -59,4 +75,4 @@ class Vector:
             return Fraction(sum(a * b for a, b in zip(self.componentes, multiplicador.componentes)))
         if isinstance(multiplicador, Fraction):
             return Vector([c * multiplicador for c in self.componentes])
-        raise TypeError("Tipo de dato inválido")
+        raise TypeError("Tipo de dato inválido!")
