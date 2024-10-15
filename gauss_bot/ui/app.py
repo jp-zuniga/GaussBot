5651174df540ab3d9a28306f1bd6b7e38e6346e3
jp-zuniga <jperez070705@gmail.com>
2024-10-15 09:01:53 -0600

    funcionalidad de guardar y cargar configuracion implementada

--- gauss_bot/ui/app.py
+++ gauss_bot/ui/app.py
@@ -1,21 +1,19 @@
-from os import path
 from fractions import Fraction
-from customtkinter import (
-    CTk as ctk,
-    set_default_color_theme,
-    set_appearance_mode
-)
+from json import dump, load
+from os import path
+
+from customtkinter import CTk as ctk
+from customtkinter import set_widget_scaling, set_default_color_theme, set_appearance_mode
 
 from gauss_bot.clases.matriz import Matriz
 from gauss_bot.clases.vector import Vector
+from gauss_bot.managers.main_manager import OpsManager
 from gauss_bot.managers.mats_manager import MatricesManager
 from gauss_bot.managers.vecs_manager import VectoresManager
-from gauss_bot.managers.main_manager import OpsManager
-
-from gauss_bot.ui.frames.nav import NavFrame
+from gauss_bot.ui.frames.config import ConfigFrame, CONFIG_PATH
 from gauss_bot.ui.frames.matrices import MatricesFrame
+from gauss_bot.ui.frames.nav import NavFrame
 from gauss_bot.ui.frames.vectores import VectoresFrame
-from gauss_bot.ui.frames.config import ConfigFrame
 
 THEMES_PATH = path.join(path.dirname(path.realpath(__file__)), "themes")
 
@@ -41,8 +39,7 @@ ops.vecs_manager = test_vecs
 class App(ctk):
     def __init__(self):
         super().__init__()
-        set_default_color_theme(path.join(THEMES_PATH, "metal.json"))
-        set_appearance_mode("dark")
+        self.load_config()
 
         self.title("GaussBot")
         self.geometry("1200x600")
@@ -54,24 +51,47 @@ class App(ctk):
         self.config_frame = ConfigFrame(self, self)
         self.matrices = MatricesFrame(self, self, self.ops_manager.mats_manager)
         self.vectores = VectoresFrame(self, self, self.ops_manager.vecs_manager)
+
+        self.frames = {
+            "matrices": self.matrices,
+            "vectores": self.vectores,
+            "config": self.config_frame
+        }
+
+        self.buttons = {
+            "matrices": self.nav_frame.matrices_button,
+            "vectores": self.nav_frame.vectores_button
+        }
+
         self.seleccionar_frame("config")
+    
+    def load_config(self):
+        with open(CONFIG_PATH, "r") as config_file:
+            self.config = load(config_file)
+        self.tema_actual = self.config["tema"]
+        self.escala_actual = self.config["escala"]
+        self.modo_actual = self.config["modo"]
 
-    def seleccionar_frame(self, name):
-        self.nav_frame.matrices_button.configure(fg_color=("gray75", "gray25") if name == "matrices" else "transparent")
-        self.nav_frame.vectores_button.configure(fg_color=("gray75", "gray25") if name == "vectores" else "transparent")
+        set_widget_scaling(self.escala_actual)
+        set_default_color_theme(path.join(THEMES_PATH, self.tema_actual))
+        set_appearance_mode(self.modo_actual)
 
-        if name == "matrices":
-            self.matrices.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.matrices.grid_forget()
-        if name == "vectores":
-            self.vectores.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.vectores.grid_forget()
-        if name == "config":
-            self.config_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.config_frame.grid_forget()
+    def save_config(self):
+        self.config["tema"] = self.tema_actual
+        self.config["escala"] = self.escala_actual
+        self.config["modo"] = self.modo_actual
+        with open(CONFIG_PATH, "w") as config_file:
+            dump(self.config, config_file, indent=4)
+
+    def seleccionar_frame(self, nombre):
+        for nombre_frame, button in self.buttons.items():
+            button.configure(fg_color=("gray75", "gray25") if nombre == nombre_frame else "transparent")
+
+        for nombre_frame, frame in self.frames.items():
+            if nombre == nombre_frame:
+                frame.grid(row=0, column=1, sticky="nsew")
+            else:
+                frame.grid_forget()
 
     def home_button_event(self):
         self.seleccionar_frame("home")
@@ -86,4 +106,5 @@ class App(ctk):
         self.seleccionar_frame("config")
 
     def quit_event(self):
+        self.save_config()
         self.quit()
--- gauss_bot/ui/frames/config.py
+++ gauss_bot/ui/frames/config.py
@@ -1,13 +1,17 @@
 from os import path
+
+from tkinter import StringVar
 from customtkinter import (
     CTkFrame as ctkFrame,
     CTkLabel as ctkLabel,
     CTkOptionMenu as ctkOptionMenu,
+    set_widget_scaling,
     set_default_color_theme,
     set_appearance_mode,
 )
 
 THEMES_PATH = path.join(path.dirname(path.dirname(path.realpath(__file__))), "themes")
+CONFIG_PATH = path.join(path.dirname(path.dirname(path.realpath(__file__))), "config.json")
 
 
 class ConfigFrame(ctkFrame):
@@ -36,27 +40,64 @@ class ConfigFrame(ctkFrame):
             "Yellow": path.join(THEMES_PATH, "yellow.json"),
         }
 
+        self.modos_dict = {
+            "Claro": "light",
+            "Oscuro": "dark",
+            "Sistema": "system",
+        }
+
+        self.escalas_float = {
+            "80%": 0.8,
+            "90%": 0.9,
+            "100%": 1.0,
+            "110%": 1.1,
+            "120%": 1.2,
+        }
+
+        self.get_tema = self.get_dict_key(self.temas_json, self.app.tema_actual)
+        self.get_modo = self.get_dict_key(self.modos_dict, self.app.modo_actual)
+        self.get_escala = self.get_dict_key(self.escalas_float, self.app.escala_actual)
+
+        self.first_tema = StringVar(value=self.get_tema.split("\\")[-1])
+        self.first_modo = StringVar(value=self.get_modo)
+        self.first_escala = StringVar(value=self.get_escala)
+
         self.temas = list(self.temas_json.keys())
-        self.modos = ["Claro", "Oscuro", "Sistema"]
+        self.escalas = list(self.escalas_float.keys())
+        self.modos = list(self.modos_dict.keys())
 
         self.temas_label = ctkLabel(self, text="Tema:")
         self.temas_label.grid(row=0, column=0, padx=20, pady=10)
-        self.desplegar_temas = ctkOptionMenu(self,values=self.temas, command=self.cambiar_tema)
+        self.desplegar_temas = ctkOptionMenu(self, variable=self.first_tema, values=self.temas, command=self.cambiar_tema)
         self.desplegar_temas.grid(row=0, column=1, padx=20, pady=10)
 
         self.modos_label = ctkLabel(self, text="Modo:")
         self.modos_label.grid(row=1, column=0, padx=20, pady=10)
-        self.desplegar_modos = ctkOptionMenu(self, values=self.modos, command=self.light_dark)
+        self.desplegar_modos = ctkOptionMenu(self, variable=self.first_modo, values=self.modos, command=self.light_dark)
         self.desplegar_modos.grid(row=1, column=1, padx=20, pady=10)
 
+        self.escala_label = ctkLabel(self, text="Escala:")
+        self.escala_label.grid(row=2, column=0, padx=20, pady=10)
+        self.desplegar_escalas = ctkOptionMenu(self, variable=self.first_escala, values=self.escalas, command=self.cambiar_escala)
+        self.desplegar_escalas.grid(row=2, column=1, padx=20, pady=10)
+
+    def cambiar_escala(self, escala_seleccionada):
+        self.app.escala_actual = self.escalas_float[escala_seleccionada]
+        set_widget_scaling(self.app.escala_actual)
+        self.app.save_config()
+
     def cambiar_tema(self, tema_seleccionado):
-        set_default_color_theme(self.temas_json[tema_seleccionado])
-        
+        self.app.tema_actual = self.temas_json[tema_seleccionado]
+        set_default_color_theme(self.app.tema_actual)
+        self.app.save_config()
+
     def light_dark(self, modo_seleccionado):
-        match modo_seleccionado:
-            case "Claro":
-                set_appearance_mode("light")
-            case "Oscuro":
-                set_appearance_mode("dark")
-            case "Sistema":
-                set_appearance_mode("system")
+        self.app.modo_actual = self.modos_dict[modo_seleccionado]
+        set_appearance_mode(self.app.modo_actual)
+        self.app.save_config()
+
+    def get_dict_key(self, dict, tema):
+        for key, value in dict.items():
+            if value == tema:
+                return key
+        return None
