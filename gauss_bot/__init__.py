278b7c3a9829efc4cbec1a95e2594dee79151aeb
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-06-24 20:47:16 -0600

    Traslado a ejecución de módulo con __main__.py

--- gauss_bot/__init__.py
+++ gauss_bot/__init__.py
@@ -6,7 +6,26 @@ operaciones con matrices y vectores, y análisis númerico.
 
 FRAC_PREC: dict[str, int] = {"prec": 100}
 
+from platform import system  # noqa
+
 from . import gui, managers, models, utils  # noqa
 
 
-__all__: list[str] = ["managers", "models", "utils", "gui"]
+def main() -> None:
+    """
+    Main loop de la aplicación.
+    """
+
+    utils.log_setup()
+    app = gui.GaussUI()
+
+    if system() == "Windows":
+        app.after(0, lambda: app.state("zoomed"))
+    else:
+        app.wm_attributes("-zoomed", True)
+
+    app.mainloop()
+
+
+__version__: str = "1.0"
+__all__: list[str] = ["main", "managers", "models", "utils", "gui"]
---
+++ gauss_bot/__main__.py
@@ -0,0 +1,9 @@
+"""
+Entry point de ejecución.
+"""
+
+from . import main
+
+
+if __name__ == "__main__":
+    main()
