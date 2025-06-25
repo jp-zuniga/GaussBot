9e45f8b0b351e8fd89f2b25d603f8d2357938a51
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-06-25 12:33:15 -0600

    KeyBindingManager documentado

--- src/managers/binding_manager.py
+++ src/managers/binding_manager.py
@@ -3,6 +3,7 @@ Implementación de KeyBindingManager, encargado de crear
 bindings a las teclas de flecha para navegar entre entries.
 """
 
+from tkinter import Event
 from typing import Optional, Union
 
 from ..gui.custom import CustomEntry
@@ -10,9 +11,7 @@ from ..gui.custom import CustomEntry
 
 class KeyBindingManager:
     """
-    Se encarga de crear todos los bindings a las teclas de flecha
-    para que se puedan navegar los entries en los módulos
-    de agregar sistemas, matrices o vectores.
+    Manager de bindings para los CustomEntry's de la aplicación.
     """
 
     def __init__(
@@ -22,51 +21,58 @@ class KeyBindingManager:
         extra_entries: Optional[tuple[CustomEntry, CustomEntry]] = None,
     ) -> None:
         """
-        Argumentos:
-        - es_matriz: True si las entries son para una matriz,
-                     False si son para un vector
+        Args:
+            es_matriz:     Indica si las entries son para ingresar una matriz.
+            entry_list:    La lista de entries que recibirán bindings.
+            extra_entries: Las entries adicionales que se deben tomar en cuenta
+                          (nombre, filas, columnas, dimensiones).
 
-        - entry_list: la lista de entries a la que se le crearán bindings
-        - extra_entries: las entries adicionales que se deben tener en cuenta
-                         (nombre, filas, columnas, dimensiones)
-
-        Errores:
-        - ValueError:
-            - si 'entry_list' o 'extra_entries' es None a la hora de crear bindings
-            - si 'entry_list' no es una lista 2D y 'es_matriz' == True
-            - si 'es_matriz' == False y 'entry_list' no es una lista 1D.
+        Raises:
+            ValueError: Si 'entry_list' o 'extra_entries' son listas vacías;
+                        si 'es_matriz' es True y 'entry_list' no es una lista 2D;
+                        si 'es_matriz' es False y 'entry_list' no es una lista 1D.
+        ---
         """
 
         self.es_matriz = es_matriz
         self.entry_list = entry_list
         self.extra_entries = extra_entries
 
-        try:
-            if self.es_matriz and not isinstance(self.entry_list[0], list):  # type: ignore
-                raise ValueError(
-                    "Para una matriz, 'entry_list' debe ser una lista bidimensional!"
-                )
+        if entry_list is None:
+            # deferir las validaciones hasta que se crean los bindings
+            return
 
-            if not self.es_matriz and isinstance(self.entry_list[0], list):  # type: ignore
-                raise ValueError(
-                    "Para un vector, 'entry_list' debe ser una lista unidimensional!"
-                )
-        except TypeError:
-            pass
+        if self.entry_list == []:
+            raise ValueError("Argumento 'entry_list' no puede ser una lista vacía.")
+        if self.extra_entries == []:
+            raise ValueError("Argumento 'extra_entries' no puede ser una lista vacía.")
+
+        if self.es_matriz and not all(isinstance(row, list) for row in self.entry_list):
+            raise ValueError(
+                "Para una matriz, 'entry_list' debe ser una lista bidimensional."
+            )
+        if not self.matriz and any(isinstance(item, list) for item in self.entry_list):
+            raise ValueError(
+                "Para un vector, 'entry_list' debe ser una lista unidimensional."
+            )
 
     def create_key_bindings(self) -> None:
         """
-        Método principal para crear los bindings de teclas de flecha necesarios.
+        Crear los keybindings de todos los CustomEntry's especificados.
+
+        Raises:
+            ValueError: Si 'entry_list' o 'extra_entries' son None.
+        ---
         """
 
         if self.entry_list is None:
-            raise ValueError("'entry_list' no puede ser None!")
+            raise ValueError("Atributo 'entry_list' no puede ser None.")
         if self.extra_entries is None:
-            raise ValueError("'extra_entries' no puede ser None!")
+            raise ValueError("Atributo 'extra_entries' no puede ser None.")
 
         if self.es_matriz:
             for i, row in enumerate(self.entry_list):
-                for j, entry in enumerate(row):  # type: ignore
+                for j, entry in enumerate(row):
                     self._bind_mat_entry(i, j, entry)
         else:
             for i, entry in enumerate(self.entry_list):
@@ -74,11 +80,16 @@ class KeyBindingManager:
 
     def _bind_mat_entry(self, row: int, column: int, entry: CustomEntry) -> None:
         """
-        Crea los bindings de teclas de flecha
-        para un entry de una matriz.
+        Crear bindings para un entry de una matriz.
+
+        Args:
+            row:    Fila del entry.
+            column: Columna del entry.
+            entry:  CustomEntry a configurar.
+        ---
         """
 
-        nombre_entry, filas_entry = self.extra_entries  # type: ignore
+        nombre_entry, filas_entry = self.extra_entries
         entry.bind("<Up>", lambda _: self._entry_move_up(row, column, filas_entry))
         entry.bind("<Down>", lambda _: self._entry_move_down(row, column, nombre_entry))
         entry.bind("<Left>", lambda _: self._entry_move_left(row, column))
@@ -87,26 +98,36 @@ class KeyBindingManager:
 
     def _bind_vec_entry(self, row: int, entry: CustomEntry) -> None:
         """
-        Crea los bindings de teclas de flecha
-        para un entry de un vector.
+        Crear bindings para un entry de un vector.
+
+        Args:
+            row:    Fila del entry.
+            entry:  CustomEntry a configurar.
+        ---
         """
 
-        nombre_entry, dimensiones_entry = self.extra_entries  # type: ignore
+        nombre_entry, dimensiones_entry = self.extra_entries
         entry.bind("<Up>", lambda _: self._entry_move_up(row, -1, dimensiones_entry))
         entry.bind("<Down>", lambda _: self._entry_move_down(row, -1, nombre_entry))
         entry.bind("<Control-Tab>", self.autocomplete_all)
 
     def _entry_move_up(self, row: int, column: int, data_entry: CustomEntry) -> None:
         """
-        Cambia el foco a la entry por encima de la actual,
+        Cambiar focus al entry encima del actual,
         o al entry de filas/dimensiones si ya se llegó al inicio.
+
+        Args:
+            row:        Fila del entry.
+            column:     Columna del entry.
+            data_entry: CustomEntry a configurar.
+        ---
         """
 
         if row > 0:
             if self.es_matriz:
-                self.entry_list[row - 1][column].focus_set()  # type: ignore
+                self.entry_list[row - 1][column].focus_set()
             else:
-                self.entry_list[row - 1].focus_set()  # type: ignore
+                self.entry_list[row - 1].focus_set()
         elif row == 0:
             if self.es_matriz:
                 KeyBindingManager.focus_filas(data_entry)
@@ -117,101 +138,118 @@ class KeyBindingManager:
         self, row: int, column: int, nombre_entry: CustomEntry
     ) -> None:
         """
-        Cambia el foco a la siguiente entry,
+        Cambiar focus al entry debajo del actual,
         o al entry de nombre si ya se llegó al final.
+
+        Args:
+            row:          Fila del entry.
+            column:       Columna del entry.
+            nombre_entry: CustomEntry a configurar.
+        ---
         """
 
-        if row < len(self.entry_list) - 1:  # type: ignore
+        if row < len(self.entry_list) - 1:
             if self.es_matriz:
-                self.entry_list[row + 1][column].focus_set()  # type: ignore
+                self.entry_list[row + 1][column].focus_set()
             else:
-                self.entry_list[row + 1].focus_set()  # type: ignore
-        elif row == len(self.entry_list) - 1:  # type: ignore
+                self.entry_list[row + 1].focus_set()
+        elif row == len(self.entry_list) - 1:
             nombre_entry.focus_set()
 
     def _entry_move_left(self, row: int, column: int) -> None:
         """
-        Cambia el foco a la entry a la izquierda,
-        o a la fila anterior si se llegó al inicio de la fila,
-        o al entry de datos iniciales si se llegó al inicio de la lista de entries.
-        * row: la fila actual
-        * column: la columna actual
-
+        Cambiar focus al entry a la izquierda;
+        a la fila anterior (si se llegó al inicio de la fila);
+        o al entry de datos iniciales (si se llegó al inicio de la lista).
         Solamente utilizado para los bindings de matrices.
+
+        Args:
+            row:    Fila del entry.
+            column: Columna del entry.
+        ---
         """
 
         if column > 0:
-            self.entry_list[row][column - 1].focus_set()  # type: ignore
+            self.entry_list[row][column - 1].focus_set()
         elif column == 0:
             if row > 0:
-                self.entry_list[row - 1][-1].focus_set()  # type: ignore
+                self.entry_list[row - 1][-1].focus_set()
             elif row == 0:
                 self.focus_last()
 
     def _entry_move_right(self, row: int, column: int) -> None:
         """
-        Cambia el foco a la entry a la derecha,
-        o al siguiente fila si ya se llegó al final de la fila,
-        o al entry de nombre si ya se llegó al final de la lista de entries.
-        * row: la fila actual
-        * column: la columna actual
-
+        Cambiar focus al entry a la derecha;
+        al siguiente fila (si ya se llegó al final de la fila);
+        o al entry de nombre (si ya se llegó al final de la lista).
         Solamente utilizado para los bindings de matrices.
+
+        Args:
+            row:    Fila del entry.
+            column: Columna del entry.
+        ---
         """
 
-        if column < len(self.entry_list[row]) - 1:  # type: ignore
-            self.entry_list[row][column + 1].focus_set()  # type: ignore
-        elif column == len(self.entry_list[row]) - 1:  # type: ignore
-            if row < len(self.entry_list) - 1:  # type: ignore
-                self.entry_list[row + 1][0].focus_set()  # type: ignore
-            elif row == len(self.entry_list) - 1:  # type: ignore
+        if column < len(self.entry_list[row]) - 1:
+            self.entry_list[row][column + 1].focus_set()
+        elif column == len(self.entry_list[row]) - 1:
+            if row < len(self.entry_list) - 1:
+                self.entry_list[row + 1][0].focus_set()
+            elif row == len(self.entry_list) - 1:
                 self.focus_first()
 
     def focus_first(self) -> None:
         """
-        Cambia el foco a la primera entry de la lista.
+        Cambiar focus a la primera entry de la lista.
         """
 
         try:
             if self.es_matriz:
-                self.entry_list[0][0].focus_set()  # type: ignore
+                self.entry_list[0][0].focus_set()
             else:
-                self.entry_list[0].focus_set()  # type: ignore
+                self.entry_list[0].focus_set()
         except IndexError:
             pass
 
     def focus_last(self) -> None:
         """
-        Cambia el foco a la última entry de la lista.
+        Cambiar focus a la última entry de la lista.
         """
 
         try:
             if self.es_matriz:
-                self.entry_list[-1][-1].focus_set()  # type: ignore
+                self.entry_list[-1][-1].focus_set()
             else:
-                self.entry_list[-1].focus_set()  # type: ignore
+                self.entry_list[-1].focus_set()
         except IndexError:
             pass
 
-    def autocomplete_all(self, event) -> str:
+    def autocomplete_all(self, event: Optional[Event] = None) -> str:
         """
-        Llama autocomplete_placeholder() para todos los entries.
+        Llamar autocomplete_placeholder() para todos los entries.
+
+        Args:
+            event: Evento de tkinter que desencadenó este método.
         """
 
         del event
         if self.es_matriz:
-            for row in self.entry_list:  # type: ignore
-                for entry in row:  # type: ignore
-                    entry.autocomplete_placeholder(event=None)  # type: ignore
+            for row in self.entry_list:
+                for entry in row:
+                    entry.autocomplete_placeholder(event=None)
         else:
-            for entry in self.entry_list:  # type: ignore
-                entry.autocomplete_placeholder(event=None)  # type: ignore
+            for entry in self.entry_list:
+                entry.autocomplete_placeholder(event=None)
         return "break"
 
     @staticmethod
     def focus_dimensiones(dimensiones_entry: CustomEntry) -> None:
         """
-        Cambia el foco al entry de ingresar dimensiones.
+        Cambiar focus al entry de ingresar dimensiones.
+
+        Args:
+            dimensiones_entry: Entry a enfocar.
+        ---
         """
 
         dimensiones_entry.focus_set()
@@ -219,7 +257,11 @@ class KeyBindingManager:
     @staticmethod
     def focus_filas(filas_entry: CustomEntry) -> None:
         """
-        Cambia el foco al entry de ingresar filas.
+        Cambiar focus al entry de ingresar filas.
+
+        Args:
+            filas_entry: Entry a enfocar.
+        ---
         """
 
         filas_entry.focus_set()
@@ -227,7 +269,11 @@ class KeyBindingManager:
     @staticmethod
     def focus_columnas(columnas_entry: CustomEntry) -> None:
         """
-        Cambia el foco al entry de ingresar columnas.
+        Cambiar focus al entry de ingresar columnas.
+
+        Args:
+            columnas_entry: Entry a enfocar.
+        ---
         """
 
         columnas_entry.focus_set()
