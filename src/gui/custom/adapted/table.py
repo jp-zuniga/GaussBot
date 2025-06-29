3b7acb635bc02ed3471a97e8edc659ccb860f9c9
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-06-29 01:24:16 -0600

    Reformatear CustomTable

--- src/gui/custom/adapted/table.py
+++ src/gui/custom/adapted/table.py
@@ -38,36 +38,16 @@ class CustomTable(ctkFrame):
         self.parent = master
         self.values = values
         self.columnconfigure(0, weight=1)
-        self.rowconfigure(2, weight=1)
+        self.rowconfigure(1, weight=1)
 
         self.top_fg = ThemeManager.theme["CTkFrame"]["top_fg_color"]
         self.fg = ThemeManager.theme["CTkFrame"]["fg_color"]
 
         ctkLabel(self, text=header, font=ctkFont(size=16, weight="bold")).grid(
-            row=0, column=0, padx=20, pady=20, sticky="new"
+            row=0, column=0, padx=20, pady=20, sticky="nsew"
         )
 
-        self.header_frame = ctkFrame(self, fg_color="transparent", border_width=0)
-
-        for j, value in enumerate(self.values[0]):
-            self.header_frame.columnconfigure(j, weight=1)
-            cell = ctkButton(
-                self.header_frame,
-                text=value,
-                fg_color="transparent",
-                text_color=ThemeManager.theme["CTkEntry"]["text_color"],
-                border_color=ThemeManager.theme["CTkFrame"]["border_color"],
-                border_width=2,
-                border_spacing=0,
-                corner_radius=8,
-                font=ctkFont(size=12, weight="bold"),
-                hover=False,
-            )
-
-            cell._text_label.configure(justify="center")  # type: ignore
-            cell.grid(row=0, column=j, padx=6 if j == 0 else (1, 0), sticky="nsew")
-
-        self.cells_frame = CustomScrollFrame(
+        self.dummy_frame = CustomScrollFrame(
             self,
             border_width=0,
             scrollbar_width=14,
@@ -75,10 +55,6 @@ class CustomTable(ctkFrame):
             fg_color="transparent",
         )
 
-        self.header_frame.grid(row=1, column=0, padx=20, sticky="nsew")
-        self.cells_frame.grid(row=2, column=0, padx=20, pady=(0, 20), sticky="nsew")
-
-        self.cells: list[list[ctkButton]] = [[]]
         self.init_table()
 
     def init_table(self):
@@ -86,30 +62,35 @@ class CustomTable(ctkFrame):
         Initialize the table.
         """
 
-        for i, row in enumerate(self.values[1:]):
+        for i, row in enumerate(self.values):
             for j, value in enumerate(row):
-                self.cells_frame.rowconfigure(i, weight=1)
-                self.cells_frame.columnconfigure(j, weight=1)
-
+                self.dummy_frame.rowconfigure(i, weight=1)
+                self.dummy_frame.columnconfigure(j, weight=1)
                 cell = ctkButton(
-                    self.cells_frame,
+                    self.dummy_frame,
                     text=value,
                     fg_color=(
                         ThemeManager.theme["CTkButton"]["fg_color"]
-                        if i == len(self.values) - 2
+                        if i == len(self.values) - 1
                         else self.top_fg
                         if i % 2 == 0
                         else self.fg
+                        if i != 0
+                        else "transparent"
                     ),
                     text_color=ThemeManager.theme["CTkEntry"]["text_color"],
                     border_color=ThemeManager.theme["CTkFrame"]["border_color"],
-                    border_width=2 if i == len(self.values) - 2 else 1,
+                    border_width=2 if i == 0 or i == len(self.values) - 1 else 1,
                     border_spacing=0,
                     corner_radius=8,
                     font=ctkFont(
-                        size=(12 if j == 0 or i == len(self.values) - 2 else 10),
+                        size=(
+                            12 if j == 0 or i == 0 or i == len(self.values) - 1 else 10
+                        ),
                         weight=(
-                            "bold" if j == 0 or i == len(self.values) - 2 else "normal"
+                            "bold"
+                            if j == 0 or i == 0 or i == len(self.values) - 1
+                            else "normal"
                         ),
                     ),
                     hover=False,
@@ -120,6 +101,7 @@ class CustomTable(ctkFrame):
                     row=i,
                     column=j,
                     padx=(0, 6) if j == 0 else (1, 0),
-                    pady=3 if i == len(self.values) - 2 else (0, 1),
+                    pady=3 if i == 0 or i == len(self.values) - 1 else (0, 1),
                     sticky="nsew",
                 )
+        self.dummy_frame.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="nsew")
