0293c4068284b320713c7a30a8b2ecdbf0fa089d
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-07-03 17:37:27 -0600

    Mejorar diseño de CustomTable

--- src/gui/custom/adapted/table.py
+++ src/gui/custom/adapted/table.py
@@ -16,7 +16,6 @@ from typing import TYPE_CHECKING
 from customtkinter import (
     CTkButton as ctkButton,
     CTkFont as ctkFont,
-    # CTkLabel as ctkLabel,
     CTkToplevel as ctkTop,
     ThemeManager,
 )
@@ -42,21 +41,24 @@ class CustomTable(CustomScrollFrame):
         from .. import IconButton
 
         super().__init__(master, border_width=3, fg_color="transparent")
-
         self.rowconfigure(1, weight=1)
         self.app = app
         self.values = values
 
         ctkButton(
             self,
+            height=30,
+            corner_radius=10,
+            border_width=2,
             text=header,
-            font=ctkFont(size=16, weight="bold"),
+            font=ctkFont(size=14, weight="bold"),
             fg_color="transparent",
             hover=False,
         ).grid(
             row=0,
             column=0,
-            columnspan=len(self.values[0]) - 1,
+            columnspan=len(self.values[0]) - 2,
+            ipadx=5,
             padx=20,
             pady=20,
             sticky="nw",
@@ -67,51 +69,37 @@ class CustomTable(CustomScrollFrame):
             self,
             self.app,
             height=30,
-            text="Exportar tabla a CSV",
-            tooltip_text="Exportar los valores de la tabla a\n "
-            + "un archivo .csv en su escritorio.",
-            font=ctkFont(weight="bold"),
-            fg_color=ThemeManager.theme["CTkButton"]["fg_color"],
+            corner_radius=10,
+            border_width=2,
+            text="Exportar a CSV",
+            tooltip_text="Exportar la tabla a un archivo .csv.",
             command=self.export_to_csv,
             swap_tooltip_colors=True,
-        ).grid(
-            row=len(self.values) + 2,
-            column=0,
-            columnspan=len(self.values[0]) - 1,
-            padx=20,
-            pady=20,
-            sticky="nw",
-        )
+        ).grid(row=0, column=len(self.values[0]) - 1, padx=20, pady=20, sticky="ne")
 
     def init_table(self):
         """
         Initialize the table.
         """
 
-        top_fg: str = ThemeManager.theme["CTkFrame"]["top_fg_color"]
-        fg: str = ThemeManager.theme["CTkFrame"]["fg_color"]
-
         for i, row in enumerate(self.values):
             for j, value in enumerate(row):
                 self.rowconfigure(i + 1, weight=1)
-                # self.columnconfigure(j, weight=1)
                 cell = ctkButton(
                     self,
                     text=value,
                     fg_color=(
                         ThemeManager.theme["CTkButton"]["fg_color"]
                         if i == len(self.values) - 1
-                        else top_fg
+                        else ThemeManager.theme["CTkFrame"]["top_fg_color"]
                         if i % 2 == 0
-                        else fg
+                        else ThemeManager.theme["CTkFrame"]["fg_color"]
                         if i != 0
                         else "transparent"
                     ),
-                    text_color=ThemeManager.theme["CTkEntry"]["text_color"],
-                    border_color=ThemeManager.theme["CTkFrame"]["border_color"],
                     border_width=2 if i == 0 or i == len(self.values) - 1 else 1,
-                    border_spacing=0,
-                    corner_radius=8,
+                    border_spacing=1,
+                    corner_radius=10,
                     font=ctkFont(
                         size=(
                             12 if j == 0 or i == 0 or i == len(self.values) - 1 else 10
@@ -129,12 +117,16 @@ class CustomTable(CustomScrollFrame):
                 cell.grid(
                     row=i + 1,
                     column=j,
-                    padx=(20, 3)
+                    padx=(20, 5)
                     if j == 0
-                    else (2, 0)
+                    else (3, 0)
                     if j != len(self.values[0]) - 1
-                    else (2, 20),
-                    pady=3 if i == 0 or i == len(self.values) - 1 else (0, 2),
+                    else (3, 20),
+                    pady=5
+                    if i == 0
+                    else (0, 2)
+                    if i != len(self.values) - 1
+                    else (3, 5),
                     sticky="nsw",
                 )
 
